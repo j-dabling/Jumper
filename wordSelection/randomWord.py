660984df03e4600f754fee77dce4f83af07c6f2d
j-dabling <84488350+j-dabling@users.noreply.github.com>
2022-02-03 14:35:27 -0700

    layed out design for randomWord.py

---
+++ wordSelection/randomWord.py
@@ -0,0 +1,5 @@
+# The word class will choose a random word, then send the number of characters in the word to the director class.
+# _complete_word The chosen word from the word list. Should be set to private.
+# guessing_word The filled-in letters that the player has guessed of the word. Missing letters are represented by '_'.
+# isCorrect(letter) Checks if letter is in complete_word. If it is then letter will be added into guessing_word and returns True.
+#                   Otherwise returns False.
