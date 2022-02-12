8f57125cbfd26dee6d6dae1089d098ab9e45300a
j-dabling <jgdabble@zoho.com>
2022-02-12 13:22:27 -0700

    added way to play again after finishing.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# Initializes director class and starts the game, accepts input from user.
 from director import director
 
+# Initializes director class and starts the game.
 def main():
     game = director()
-    game.main()
+    game.start()
 
-    
+    again = input('Would you like to play again? (y/n)')
+    if again.lower() == 'y':
+        main()
+    else:
+        print('Thank you for playing!')
 
 main()
